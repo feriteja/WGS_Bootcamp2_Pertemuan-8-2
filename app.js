01b51b7d65dffef91f06b0b8979925dd3cc8d5aa
feriteja <feriteja@gmail.com>
2022-07-13 11:01:53 +0700

    express init

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
-const http = require("http");
+// const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const goToPage = (page, res) => {
-  fs.readFile(page, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write("error:page not found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-};
+// const goToPage = (page, res) => {
+//   fs.readFile(page, (err, data) => {
+//     if (err) {
+//       res.writeHead(404);
+//       res.write("error:page not found");
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+//   });
+// };
 
-http
-  .createServer((req, res) => {
-    const url = req.url;
+app.get("/", (req, res) => {
+  res.send("hallo");
+});
 
-    res.writeHead(200, { "Content-type": "text/html" });
+app.get("/about", (req, res) => {
+  res.send("about page");
+});
 
-    if (url === "/about") {
-      goToPage("./about.html", res);
-    } else if (url === "/contact") {
-      goToPage("./contact.html", res);
-    } else {
-      goToPage("./index.html", res);
-    }
-  })
-  .listen(3000, () => {
-    console.log("server run on port 3000");
-  });
+app.get("/contact", (req, res) => {
+  res.send("contact page");
+});
+
+app.use("/", (req, res) => {
+  res.status(404).send("test");
+});
+
+app.listen(port, () => {
+  console.log(`your server listening on port ${port}`);
+});
+
+// http
+//   .createServer((req, res) => {
+//     const url = req.url;
+
+//     res.writeHead(200, { "Content-type": "text/html" });
+
+//     if (url === "/about") {
+//       goToPage("./about.html", res);
+//     } else if (url === "/contact") {
+//       goToPage("./contact.html", res);
+//     } else {
+//       goToPage("./index.html", res);
+//     }
+//   })
+//   .listen(3000, () => {
+//     console.log("server run on port 3000");
+//   });
